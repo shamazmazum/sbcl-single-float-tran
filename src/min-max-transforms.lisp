40dbb4e9df20aab06b474c62b3c511a9d1d7e06f
Vasily Postnicov <shamaz.mazum@gmail.com>
2025-01-19 19:51:54 +0300

    Add min/max transforms for any number of arguments

--- src/min-max-transforms.lisp
+++ src/min-max-transforms.lisp
@@ -11,83 +11,50 @@
 ;; Remove source transforms for min and max
 (setf (sb-int:info :function :source-transform 'min) nil
       (sb-int:info :function :source-transform 'max) nil)
+
 ;; MIN and MAX share the same FUN-INFO for some reason,
 ;; so we need to redefine it
 (sb-c:defknown (max min) (real &rest real) real
   (sb-c:movable sb-c:foldable sb-c:flushable)
   :overwrite-fndb-silently t)
 
-;; NB: Order matters
+;; Two-arg helpers
+(sb-c:defknown (two-arg-max two-arg-min) (real real) real
+  (sb-c:movable sb-c:foldable sb-c:flushable))
 
-;; Two arg transforms
-(defun two-arg-min-max-transform-body (compare-op)
-  (alex:with-gensyms (arg1 arg2)
-    `(let ((,arg1 x)
-           (,arg2 y))
-       (if (,compare-op ,arg1 ,arg2)
-           ,arg1 ,arg2))))
+(macrolet ((frob (name two-arg-op)
+             `(sb-c:define-source-transform ,name (arg0 &rest rest)
+                (if (null rest)
+                    `(values (the real ,arg0))
+                    (labels ((expand (arg0 &rest rest)
+                               (if (null rest)
+                                   arg0
+                                   (sb-impl::once-only ((arg0 arg0)
+                                                        (acc (apply #'expand rest)))
+                                     `(,',two-arg-op ,acc ,arg0)))))
+                      (apply #'expand arg0 rest))))))
+  (frob max two-arg-max)
+  (frob min two-arg-min))
 
-(macrolet ((def-min-max-transform (op compare-op)
-             `(sb-c:deftransform ,op ((x y) (real real) *)
-                (two-arg-min-max-transform-body ',compare-op))))
-  (with-silent-transform-overwrite
-    (def-min-max-transform min <=)
-    (def-min-max-transform max >=)))
-
-(macrolet ((def-min-max-transform (op single-op double-op)
-             `(progn
-                (sb-c:deftransform ,op ((x y) (single-float real) *)
-                  '(,single-op x (coerce y 'single-float)))
-                (sb-c:deftransform ,op ((x y) (real single-float) *)
-                  '(,single-op (coerce x 'single-float) y))
-                (sb-c:deftransform ,op ((x y) (double-float real) *)
-                  '(,double-op x (coerce y 'double-float)))
-                (sb-c:deftransform ,op ((x y) (real double-float) *)
-                  '(,double-op (coerce x 'double-float) y)))))
-  (with-silent-transform-overwrite
-    (def-min-max-transform min %minf %mind)
-    (def-min-max-transform max %maxf %maxd)))
-
-;; Three arg transforms.
-;;
-;; FIXME: MIN and MAX on floating point numbers are not commutative
-;; and associative (because of NaNs), but MIN and MAX on (AND REAL
-;; (NOT FLOAT)) should be.
-(defun three-arg-min-max-transform-body (compare-op)
-  (alex:with-gensyms (arg1 arg2 arg3 tmp)
-    `(let* ((,arg1 x)
-            (,arg2 y)
-            (,arg3 z)
-
-            (,tmp (if (,compare-op ,arg1 ,arg2)
-                      ,arg1 ,arg2)))
-       (if (,compare-op ,tmp ,arg3)
-           ,tmp ,arg3))))
-
-(macrolet ((def-min-max-transform (op compare-op)
-             `(sb-c:deftransform ,op ((x y z) (real real real) *)
-                (three-arg-min-max-transform-body ',compare-op))))
-  (with-silent-transform-overwrite
-    (def-min-max-transform min <=)
-    (def-min-max-transform max >=)))
-
-(macrolet ((def-min-max-transform (op single-op double-op)
-             `(progn
-                (sb-c:deftransform ,op ((x y z) (single-float real real) *)
-                  '(,single-op (,single-op x (coerce y 'single-float)) (coerce z 'single-float)))
-                (sb-c:deftransform ,op ((x y z) (real single-float real) *)
-                  '(,single-op (,single-op (coerce x 'single-float) y) (coerce z 'single-float)))
-                (sb-c:deftransform ,op ((x y z) (real real single-float) *)
-                  '(,single-op (,single-op (coerce x 'single-float) (coerce y 'single-float)) z))
-
-                (sb-c:deftransform ,op ((x y z) (double-float real real) *)
-                  '(,double-op (,double-op x (coerce y 'double-float)) (coerce z 'double-float)))
-                (sb-c:deftransform ,op ((x y z) (real double-float real) *)
-                  '(,double-op (,double-op (coerce x 'double-float) y) (coerce z 'double-float)))
-                (sb-c:deftransform ,op ((x y z) (real real double-float) *)
-                  '(,double-op (,double-op (coerce x 'double-float) (coerce y 'double-float)) z)))))
-  (with-silent-transform-overwrite
-    (def-min-max-transform min %minf %mind)
-    (def-min-max-transform max %maxf %maxd)))
-
-;; Four arguments and more are compiled as a regular function call.
+(macrolet ((frob (name double-op single-op real-op)
+             `(sb-c:deftransform ,name ((x y) (real real) *)
+                (flet ((args-have-type-p (spec)
+                         (let ((spec-type (sb-kernel:specifier-type spec)))
+                           (or (sb-kernel:csubtypep (sb-c::lvar-type x) spec-type)
+                               (sb-kernel:csubtypep (sb-c::lvar-type y) spec-type))))
+                       (args-do-not-have-type-p (spec)
+                         (let ((spec-type (sb-kernel:specifier-type spec)))
+                           (and (eq (sb-kernel:type-intersection(sb-c::lvar-type x) spec-type)
+                                    sb-kernel:*empty-type*)
+                                (eq (sb-kernel:type-intersection(sb-c::lvar-type y) spec-type)
+                                    sb-kernel:*empty-type*)))))
+                  (cond
+                    ((args-have-type-p 'double-float)
+                     '(,double-op (sb-kernel:%double-float x) (sb-kernel:%double-float y)))
+                    ((and (args-have-type-p 'single-float)
+                          (args-do-not-have-type-p 'double-float))
+                     '(,single-op (sb-kernel:%single-float x) (sb-kernel:%single-float y)))
+                    (t
+                     '(if (,real-op x y) x y)))))))
+  (frob two-arg-min %mind %minf <)
+  (frob two-arg-max %maxd %maxf >))
